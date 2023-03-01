03bbd74ba0352385ee9301934eeabb0c8f8b8cbc
Matheus Popolin <matheuspopolin@hotmail.com>
2023-03-01 20:39:15 -0300

    feat: confugurate routes and global errors

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,14 @@
 import express, { Application } from "express";
+import "express-async-errors";
+import { errorHandler } from "./errors";
+import { moviesRoutes } from "./routes/movies.routes";
 
-export const app: Application = express();
+const app: Application = express();
 
 app.use(express.json());
+
+app.use("/movies", moviesRoutes);
+
+app.use(errorHandler);
+
+export default app;
