855136656d7b903f6e0300fe8e5d49134ee283fc
Matheus Popolin <matheuspopolin@hotmail.com>
2023-03-01 20:40:19 -0300

    feat: run migrations

--- src/migrations/1677604812241-createTableMovies.ts
+++ src/migrations/1677604812241-createTableMovies.ts
@@ -1,14 +1,22 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class createTableMovies1677604812241 implements MigrationInterface {
-    name = 'createTableMovies1677604812241'
+  name = "createTableMovies1677604812241";
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "movies" ("id" SERIAL NOT NULL, "name" character varying(50) NOT NULL, "description" text, "duration" integer NOT NULL, "price" integer NOT NULL, CONSTRAINT "UQ_3a794f850bd3e432c46b3faa913" UNIQUE ("name"), CONSTRAINT "PK_c5b2c134e871bfd1c2fe7cc3705" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "movies" (
+        "id" SERIAL NOT NULL,
+        "name" character varying(50) NOT NULL,
+        "description" text,
+        "duration" integer NOT NULL,
+        "price" integer NOT NULL,
+        CONSTRAINT "UQ_3a794f850bd3e432c46b3faa913" UNIQUE ("name"),
+        CONSTRAINT "PK_c5b2c134e871bfd1c2fe7cc3705" PRIMARY KEY ("id"))`
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "movies"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "movies"`);
+  }
 }
